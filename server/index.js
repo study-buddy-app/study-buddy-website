1b5cd7c387e661b636a6098eb03d7c711cb231d0
Skyler Boyack <skylerboyack@gmail.com>
2021-06-16 11:54:48 -0600

    added main and proxy to package.json

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
-const cors = require("cors")
+// const cors = require("cors")
 
 const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env
 

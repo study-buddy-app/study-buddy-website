0fc757161aa4e93fa938fe88388c63083cf98da8
Skyler Boyack <skylerboyack@gmail.com>
2021-06-16 16:48:14 -0600

    made a  header skeleton

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import {Link, withRouter} from 'react-router-dom'
+import '../header/Header.css'
+
+
+const Header = () => {
+    return (
+    <header>
+            <Link to='/'><h1>HomePage</h1></Link>
+            <Link to='/Studentauth'><h1>Login</h1></Link>
+  </header>)
+}
+
+    export default withRouter(Header)

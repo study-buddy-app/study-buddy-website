db993b137eb8aef64b43a1479af339c3e03bbd22
Skyler Boyack <skylerboyack@gmail.com>
2021-06-15 19:09:21 -0600

    added server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const cors = require("cors")
+
+const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env
+
+
+const app = express()
+
+app.use(express.json())
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 1000 *60 *60 *24}
+}))
+
+
+// massive({
+//   connectionString: CONNECTION_STRING,
+//   ssl: {rejectUnauthorized: false}
+// })
+// .then(db => {
+//   app.set('db', db)
+//   console.log("Database Connected")
+// }).catch(err => console.log(err))
+
+app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))

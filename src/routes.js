9f64bad434d8224442dad9fd03d16871dde42cb1
Yemik2707 <81994343+Yemik2707@users.noreply.github.com>
2021-06-16 17:27:39 -0400

    loginpage

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import {Switch, Route} from 'react-router-dom'
+import Landing from './components/Landing'
+import Studentauth from './components/Studentauth' 
+
+export default (
+    <Switch>
+        <Route exact path = '/landing' component={Landing} />
+        <Route path = '/Studentauth' component={Studentauth} />
+    </Switch>
+)

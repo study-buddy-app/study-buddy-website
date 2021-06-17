c135a1933aa974ac04f28138f9d47088e539740b
Yemik2707 <81994343+Yemik2707@users.noreply.github.com>
2021-06-17 18:07:48 -0400

    login page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import {Switch, Route} from 'react-router-dom'
 import Landing from './components/landingPage/Landing'
-import Studentauth from './components/studentAuth/Studentauth' 
+import Studentlogin from './components/studentLogin/Studentlogin'
+import Dashboard from './components/dashboard/Dashboard'
+import Tutorlogin from './components/tutorLogin/Tutorlogin'
+
 
 export default (
     <Switch>
         <Route exact path = '/' component={Landing} />
-        <Route path = '/Studentauth' component={Studentauth} />
+        <Route path = '/studentlogin' component={Studentlogin} />
+        <Route path = '/dashboard' component={Dashboard} />
+        <Route path = '/tutorlogin' component={Tutorlogin} />
     </Switch>
 )

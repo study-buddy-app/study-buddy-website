203997eede9182cc95561adccdabf006b6095588
Skyler Boyack <skylerboyack@gmail.com>
2021-06-18 19:21:56 -0600

    fixed some merege issues

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const session = require('express-session')
 const cors = require("cors")
 const authCtrl = require('./controllers/authCtrl')
 const studentCtrl = require('./controllers/studentCtrl')
-const turorCtrl = require('./controllers/tutorCtrl')
+const tutorCtrl = require('./controllers/tutorCtrl')
 const backpackCtrl = require('./controllers/backpackCtrl')
 const subjectCtrl = require( './controllers/subjectCtrl')
 
@@ -35,16 +35,21 @@ massive({
   app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
 }).catch(err => console.log(err))
 
-//ANDPOINTS
+//ENDPOINTS
 //auth
 app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
 app.get('/auth/logout', authCtrl.logout)
 app.get('/auth/user', authCtrl.getUser)
 //student
+app.put('/student/profile', studentCtrl.updateStudent)
+app.get('/student/session/tutors', studentCtrl.getTutors)
+app.post('/student/session/tutor', studentCtrl.addTutor)
+app.delete('/student/session/tutor', studentCtrl.deleteTutor)
 //tutor
-//session
+app.put('/tutor/profile', tutorCtrl.updateProfile)
 
+//session
 app.use(cors())
 //Payment Ctrl
 app.post('/payment', cors(), paymentCtrl.addPayment)
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { HashRouter } from "react-router-dom"
 import {Provider} from 'react-redux'
-import {HashRouter} from 'react-router-dom'
 import store from './redux/store'
 
 ReactDOM.render(

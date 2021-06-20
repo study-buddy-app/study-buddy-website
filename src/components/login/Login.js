3fdc4e7fc3660239bb8a6ac809d0fb71e34d5f83
Yemik2707 <81994343+Yemik2707@users.noreply.github.com>
2021-06-19 21:39:29 -0400

    fixed the routes

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import './Studentlogin.scss'
+import './Login.scss'
 
 
 const Studentlogin = () => {
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,15 @@
 import {Switch, Route} from 'react-router-dom'
 import Landing from './components/landingPage/Landing'
-import Studentlogin from './components/studentLogin/Studentlogin'
+import Login from './components/login/Login'
 import Dashboard from './components/dashboard/Dashboard'
-import Tutorlogin from './components/tutorLogin/Tutorlogin'
 import Registration from './components/studentAuth/Registration'
 
 export default (
     <Switch>
         <Route exact path = '/' component={Landing} />
-        <Route path = '/studentlogin' component={Studentlogin} />
+        <Route path = '/login' component={Login} />
         <Route path = '/registration' component={Registration} />
         <Route path = '/dashboard' component={Dashboard} />
-        <Route path = '/tutorlogin' component={Tutorlogin} />
+       
     </Switch>
 )

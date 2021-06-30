a3f5f5d50f2ccd1965fef960f3540800c06b68cd
Skyler Boyack <skylerboyack@gmail.com>
2021-06-30 13:33:54 -0600

    tutor header now loads when logged in as a tutor

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,14 +6,15 @@ import Logout from '../login/logout'
 
 
 
+
+
 const Header = () => {
   const [click, setClick] = useState(false)
-
-
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
+  const [usertype, setUsertype] = useState('')
 
-    return (
+    if(usertype === ''){return (
       <>
     <header className='header'>    
         <div className='header-container'>
@@ -62,7 +63,57 @@ const Header = () => {
       </div> 
    </header>
    </>
-  )
+  )}
+   if(usertype === 'tutor'){return (
+      <>
+    <header className='header'>    
+        <div className='header-container'>
+          <Link to='/' onClick={closeMobileMenu}>
+          <img   className ="logo"src='https://study-buddy-bucket.s3.us-east-2.amazonaws.com/Study+Buddy+logo+resize.png' alt='Study Buddy logo'/>
+          </Link>  
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+           </div>
+           <div>
+            <input className='searchBar' placeholder='Search'/>
+          </div>
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+           <li className='nav-item'>
+           <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                <h3>Home</h3> 
+               </Link>
+          </li>
+
+          <li className='nav-item'>
+              <Link
+                to='/aboutus'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                <h3>About Us</h3>
+              </Link>
+          </li>
+          <li className='nav-item'>
+              <Link
+                to='/buddyup'
+                className='nav-links'
+                onClick= {closeMobileMenu}
+                >
+                <h3>Meet Up</h3>
+              </Link>
+          </li>
+          <li className='nav-links'>
+            <Logout closeMobileMenu={closeMobileMenu} />  
+            <li to='/tutordash'> <i className="far fa-user-circle"></i></li>
+          </li>
+    
+      
+        </ul>
+ 
+      </div> 
+   </header>
+   </>
+  )}
 }
 
    

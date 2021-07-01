0a01d546ad004637d8323ba7428401cea4ca623a
circuitmakr <circuitmakr@gmail.com>
2021-06-30 18:22:52 -0700

    dashboard schedule entry added

--- db/sessions/get_user_latest_session.sql
+++ db/sessions/get_user_latest_session.sql
@@ -1,4 +1,4 @@
-SELECT sch.event_start, s.subject, sch.buddy_choice 
+SELECT sch.event_start, s.subject, sch.buddy_choice, sch.description 
 FROM tbl_schedule sch
 JOIN tbl_subject s
 ON s.subject_id::VARCHAR = sch.sch_subject

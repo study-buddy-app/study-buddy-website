973254677e48ed107eeda1241ca98b9ee0074a8a
circuitmakr <circuitmakr@gmail.com>
2021-06-30 23:52:04 -0700

    alphabetized backpack listings

--- db/backpack/add_to_backpack.sql
+++ db/backpack/add_to_backpack.sql
@@ -7,4 +7,4 @@ FROM tbl_student_subject_junction sj
 JOIN tbl_subject s 
 ON sj.subject_id = s.subject_id
 WHERE sj.student_id = $1
-ORDER BY sj.subject_id;
+ORDER BY s.subject;
--- db/backpack/delete_subject_from_backpack.sql
+++ db/backpack/delete_subject_from_backpack.sql
@@ -3,4 +3,4 @@ WHERE student_id = $1 AND subject_id = $2;
 SELECT  sj.subject_id,  s.subject FROM tbl_student_subject_junction sj 
 JOIN tbl_subject s ON sj.subject_id = s.subject_id
 WHERE sj.student_id = $1
-ORDER BY sj.student_subject_id;
+ORDER BY s.subject;
